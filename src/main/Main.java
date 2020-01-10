ac364cc6803b4950206c4e7b0cc322c1536f591a
Ukyuma <fetinosay92i@gmail.com>
2020-01-10 19:35:34 +0100

    Implémentation de l'afficheur du chiffre 5 en ASCII

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,13 @@
 package main;
 
+import utils.AfficheurAscii5;
+
 public class Main {
 
 	public static void main(String[] args) {
+		
+		AfficheurAscii5 asci = new AfficheurAscii5();
+		asci.afficheascii();
 
 	}
 
