ea69acfacb8ef44fb821357c7ea9a37886c16b51
pooetitu <pooetitu@gmail.com>
2020-01-10 19:16:03 +0100

    Implémentation de l'afficheur de la table de 5

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,11 @@
 package main;
 
+import utils.AfficheurX5;
+
 public class Main {
 
 	public static void main(String[] args) {
-
+		AfficheurX5.afficheX5();
 	}
 
 }
--- src/utils/AfficheurX5.java
+++ src/utils/AfficheurX5.java
@@ -1,5 +1,9 @@
 package utils;
 
 public class AfficheurX5 {
-
+	public static void afficheX5() {
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(i*5);
+		}
+	}
 }
